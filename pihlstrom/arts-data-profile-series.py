7fd82d012f243db0c0df8e368d9b560e9b74ba19
rpi11 <91921953+rpi11@users.noreply.github.com>
2025-03-26 17:44:22 -0400

    late addition

--- pihlstrom/arts-data-profile-series.py
+++ pihlstrom/arts-data-profile-series.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 
 # Specify folder path and file name for saving out data and informative logging
 file_path = "pihlstrom"
-resource_name =  "bjs-fed-prison-mortality"
+resource_name =  "arts-data-profile-series"
 # URL to scrape
-url = 'https://bjs.ojp.gov/taxonomy/term/deaths-custody'
+url = 'https://www.arts.gov/impact/research/arts-data-profile-series'
 # File types to download from URL
 file_types = ('.zip')
 
@@ -20,26 +20,20 @@ if __name__ == '__main__':
     # Set up soup object to parse text; can toggle dynamic=True, headless=False options to see Selenium functionality
     soup = utils.set_up_soup(url=url, logger=logger)
 
-    for page_num in range(6):
-
-        driver.get(url + f"?page={page_num}")
-        articles = driver.find_elements(By.XPATH, '//*[@id="block-ojp-content"]/article')
+    for i in range(2):
+        driver.get(url + f"?page={i+1}")
+        articles = driver.find_elements(By.TAG_NAME, "article")
+        print(len(articles))
 
         for index, article in enumerate(articles, start=1):
             try:
                 link_element = article.find_element(By.XPATH, './/a')
                 href = link_element.get_attribute("href")
 
-                utils.download_files(logger, utils.set_up_soup(url=href, logger=logger), os.path.join(file_path, resource_name), href, file_types = (".zip"))
-
+                utils.download_files(logger, utils.set_up_soup(url=href, logger=logger), os.path.join(file_path, resource_name), href, file_types = (".zip",".xlsx",".pdf"))
+                print(f"Article {index}: {href}")
             except:
                 print(f"Article {index}: No link found")
-        
 
     # Close WebDriver
     driver.quit()
-
-
-
-
-
